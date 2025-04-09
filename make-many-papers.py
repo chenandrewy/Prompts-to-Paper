3596f671c5d7119115c1314a340c7a49ef434f2e
Andrew Y. Chen <65506481+chenandrewy@users.noreply.github.com>
2025-04-09 13:10:20 -0400

    Update make-many-papers.py

--- make-many-papers.py
+++ make-many-papers.py
@@ -9,10 +9,10 @@ import yaml
 import sys
 
 # User input
-# plan_name = 'plan0408-piecewise'
-plan_name = 'plan0000-test'
+plan_name = 'plan0408-piecewise'
+# plan_name = 'plan0000-test'
 run_start = 1
-run_end = 3
+run_end = 5
 
 # Extract plan number and name
 temp_num, temp_name = plan_name.split("plan")[1].split("-")
